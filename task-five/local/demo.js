e6ba076c47ca3d463d3c05063a1b45a49c7a2de0
wanghaiming <554825929@qq.com>
2018-04-07 21:31:15 +0800

    task-five the third commit

--- task-five/local/demo.js
+++ task-five/local/demo.js
@@ -10,9 +10,9 @@ fs.access("dirName",(err) =>{
         fs.mkdirSync('css') // mkdir css
         fs.mkdirSync('js') // mkdir js
        
-        fs.writeFileSync("./index.html", "")
-        fs.writeFileSync("css/style.css", "")
-        fs.writeFileSync("./js/main.js", "")   
+        fs.writeFileSync("./index.html", "<!DOCYPE>\n<title>Hello</title>\n<h1>Hi<h1>")
+        fs.writeFileSync("css/style.css", "h1{color: red;}")
+        fs.writeFileSync("./js/main.js", "var string =\"Hello World\"\nalert(string)")   
     }
     else{
         console.log("the dir has exisited")
