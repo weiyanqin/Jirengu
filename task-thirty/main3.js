ea569067c3a5ddb7fd8a74e92d8f03c928418436
weiyanqin <wanghaiming723@foxmail.com>
2018-05-27 22:15:53 +0800

    task-thirty the second commit

--- task-thirty/main3.js
+++ task-thirty/main3.js
@@ -1,11 +1,25 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-
-setTimeout(() => {
-    $('.images > img:nth-child(1)').removeClass('current').addClass('leave').one('transitionend', (e) => {
+let n 
+初始化()
+setInterval(() => {
+    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave').one('transitionend',(e)=>{
         $(e.currentTarget).removeClass('leave').addClass('enter')
     })
-    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
+    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    n += 1
 
 }, 3000)
 
+function x(n) {
+    if (n > 3) {
+        n = n % 3
+        if (n === 0) {
+            n = 3
+        }
+    }
+    return n
+}
+ 
+function 初始化(){
+    n  = 1
+    $(`.images > img:nth-child(${n})`).addClass('current').siblings().addClass('enter')
+}
