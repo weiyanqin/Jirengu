757dcfa4c273acdde74518afe6cafaaf24063554
weiyanqin <wanghaiming723@foxmail.com>
2018-06-01 08:14:12 +0800

    task-thirty the third commit

---
+++ task-thirty/main4.js
@@ -0,0 +1,69 @@
+let n
+初始化()
+let timer = setInterval(() => {
+    makeLeave(getImage(n))
+        .one('transitionend', (e) => {
+            makeEnter($(e.currentTarget))
+        })
+    makeCurrent(getImage(n + 1))
+    n += 1
+}, 1000)
+
+document.addEventListener('visibilitychange', function (e) {
+    if (doucment.hidden) {
+        window.clearInterval(timer)
+    } else {
+        timer = setInterval(() => {
+            makeLeave(getImage(n))
+                .one('transitionend', (e) => {
+                    makeEnter($(e.currentTarget))
+                })
+            makeCurrent(getImage(n + 1))
+            n += 1
+        }, 1000)
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+// 下面可以不看
+
+function getImage(n) {
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n) {
+    if (n > 3) {
+        n = n % 3
+        if (n === 0) {
+            n = 3
+        }
+    } // n = 1 2 3
+    return n
+}
+
+function 初始化() {
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current')
+}
+
+function makeLeave($node) {
+    return $node.removeClass('enter current').addClass('leave')
+}
+
+function makeEnter($node) {
+    return $node.removeClass('leave current').addClass('enter')
+}
