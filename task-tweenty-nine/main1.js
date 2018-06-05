84f6cc646355058bb7d94ad91149b0916082837b
weiyanqin <wanghaiming723@foxmail.com>
2018-06-05 21:13:10 +0800

    task-tweenty-nine the second commit

---
+++ task-tweenty-nine/main1.js
@@ -0,0 +1,16 @@
+$(p1).on('click', function () {
+    $(images).css({
+        transform: 'translateX(0)'
+    })
+})
+
+$(p2).on('click', function () {
+    $(images).css({
+        transform: 'translateX(-400px)'
+    })
+})
+$(p3).on('click',function(){
+    $(images).css({
+        transform:'translateX(-800px)'
+    })
+})
