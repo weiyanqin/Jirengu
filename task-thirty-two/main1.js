1826b378911baeca284b37c2e02337c0cb1d0bca
weiyanqin <wanghaiming723@foxmail.com>
2018-06-05 21:15:27 +0800

    task-thirty-two the third commit

--- task-thirty-two/main1.js
+++ task-thirty-two/main1.js
@@ -1,3 +1,11 @@
-clickMe.addEventListener('click',function(){
-    popover.style.display = 'block'
+$(clickMe).on('click', function () {
+    $(popover).show()
+    console.log(1)
+    $(document).one('click', function () {
+        $(popover).hide()
+        console.log(1)
+    })
+})
+$(wrapper).on('click', function (e) {
+    e.stopPropagation()
 })
