c20df1852d263d905832bba8cdfeba6095086e96
weiyanqin <wanghaiming723@foxmail.com>
2018-06-07 10:38:03 +0800

    task-thirty-two the fourth commit

--- task-thirty-two/main1.js
+++ task-thirty-two/main1.js
@@ -1,11 +1,30 @@
+// $(clickMe).on('click', function () {
+//     $(popover).show()
+//     console.log(1)
+//     setTimeout(function () {
+//         $(clickMe).one('click', function () {
+//             $(popover).hide()
+//             console.log(2)
+//         })
+//     }, 0)
+// })
+
+var n = 0
+
 $(clickMe).on('click', function () {
+    n += 1
     $(popover).show()
-    console.log(1)
-    $(document).one('click', function () {
-        $(popover).hide()
-        console.log(1)
+    setTimeout(function () {
+        $(document).on('click', function () {
+            $(popover).hide()
+            n = 0
+        })
+    },0)
+
+    $(wrapper).on('click',function(e){
+        e.stopPropagation()
     })
-})
-$(wrapper).on('click', function (e) {
-    e.stopPropagation()
+    if (n % 2 === 0) {
+        $(popover).hide()
+    }
 })
